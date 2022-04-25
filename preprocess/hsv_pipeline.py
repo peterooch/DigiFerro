09857aec909d2655162805b39fc64b24e4eaf2b2
Baruch Rutman <peterooch@gmail.com>
2022-04-25 13:57:19 +0300

    Tidy

--- preprocess/hsv_pipeline.py
+++ preprocess/hsv_pipeline.py
@@ -1,11 +1,15 @@
+from typing import List
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.exposure import match_histograms
 
-RED_LOWER = np.uint8([0, 0, 200])
-RED_UPPER = np.uint8([255, 255, 255])
+# Spalling fragment hsv range
+SPALLING_LOWER = np.uint8([0, 0, 200])
+SPALLING_UPPER = np.uint8([255, 255, 255])
 
+# Histogram baseline image
 BASELINE = cv2.imread('preprocess/baseline.jpg')
 
 def create_mask(file_name):
@@ -15,11 +19,11 @@ def create_mask(file_name):
     img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
     img = np.uint8(match_histograms(img, BASELINE))
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, RED_LOWER, RED_UPPER)
+    mask = cv2.inRange(hsv, SPALLING_LOWER, SPALLING_UPPER)
 
     return mask
 
-def to_countour(mask):
+def contour_dims(mask) -> List[float]:
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     img = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
     dims = []
@@ -29,10 +33,10 @@ def to_countour(mask):
         cv2.drawContours(img, [pts], -1, (0, 255, 0), 1)
         dims.append(np.linalg.norm(pts[0] - pts[2]))
 
-    plt.imshow(img)
-    plt.show()
+    #plt.imshow(img)
+    #plt.show()
     return sorted(dims, reverse=True)
 
 if __name__ == "__main__":
     mask = create_mask('preprocess/baseline.jpg')
-    print(to_countour(mask))
+    print(contour_dims(mask))
