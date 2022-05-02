6cf34d54ce911ddc15994aad1f79ed29eb2de977
Roiamz <57364615+Roiamz@users.noreply.github.com>
2022-05-02 21:36:59 +0300

    Report class

--- Report.py
+++ Report.py
@@ -1,18 +1,17 @@
 class Report:
     def __init__(self, squadron, hangar, sampleDate,timeSinceOverhaul, 
-                iron, titanum, otherMetals, scale, testNumber, tailNumber,
-                partNumber, flightNumber,conclusion):
+                iron, titanium, otherMetals, scale, testNumber, tailNumber,
+                partNumber,conclusion):
         self.testNumber = testNumber
         self.tailNumber = tailNumber
         self.partNumber = partNumber
-        self.flightNumber = flightNumber
         self.conclusion = conclusion
         self.squadron = squadron
         self.hangar = hangar
         self.sampleDate = sampleDate
         self.timeSinceOverhaul = timeSinceOverhaul
         self.iron = iron
-        self.titanum = titanum 
+        self.titanium = titanium 
         self.otherMetals = otherMetals
         self.scale = scale
-        
+    
--- openfile.py
+++ openfile.py
@@ -5,6 +5,7 @@ from PyQt5 import uic
 
 from imageproc import image
 from util import resource_path
+from Report import Report
 
 class OpenFileWindow(QDialog):
     def __init__(self, parent):
@@ -34,6 +35,20 @@ class OpenFileWindow(QDialog):
 
     def ok(self):
         self.hide()
+        testNumber = self.sampleNumLabel.text()
+        squadron = self.squadronEdit.text()
+        hangar = self.hangarEdit.text()
+        sampleDate = self.sampleDateEdit.date()
+        tailNumber = self.tailNumEdit.text()
+        partNumber = self.partNumEdit.text()
+        timeSinceOverhaul = self.overhaulTimeEdit.value()
+        iron = self.ironEdit.text()
+        titanium = self.titaniumEdit.text()
+        otherMetals = self.otherMetalsComboBox.currentText()
+        scale = self.scaleEdit.value()
+        report = Report(squadron, hangar, sampleDate, timeSinceOverhaul,
+        iron, titanium, otherMetals, scale, testNumber,
+        tailNumber, partNumber, 80)
         img = image(self.imagePathEdit.text())
         self.parent.set_image(img)
         self.parent.show_image('original')
